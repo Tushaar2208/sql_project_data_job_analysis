6a1cea9a1e487131a80a0b17675b67a7e6d5d57d
Tushaar2208 <tushaarkapil22@gmail.com>
2026-02-08 00:13:10 +0530

    filter out skills with low demand

--- project_sql/4_top_paying_skills.sql
+++ project_sql/4_top_paying_skills.sql
@@ -1,18 +1,21 @@
 -- Question: What are the top skills based on salary ?
 SELECT 
     skills,
-    ROUND(AVG(salary_year_avg),0) AS avg_salary
-FROM job_postings_fact
+    ROUND(AVG(salary_year_avg), 0) AS avg_salary
+FROM 
+    job_postings_fact
     INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
     INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
-WHERE 
+WHERE
     job_title_short = 'Data Analyst' AND
     salary_year_avg IS NOT NULL
 GROUP BY 
     skills
+HAVING 
+    COUNT(skills_job_dim.job_id) > 10 
 ORDER BY 
     avg_salary DESC
-LIMIT 25
+LIMIT 25;
 
 /*
  Here's a breakdown of the results for top paying skills for Data Analysts:
